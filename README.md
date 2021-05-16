2c302a9bd8569aad205781afedec446836d2ad7c
Devdutt Shenoi <devdutt@outlook.in>
2021-05-16 14:34:24 +0530

    restructure as a rust repo

--- README.md
+++ README.md
@@ -7,3 +7,8 @@
 | 3 | [Awesome new Python learnings](posts/opensalve-pipenv.md)				            | March 8, 2019		|
 | 2 | [How I moved OpenSalve from using Virtualenv to Pipenv](posts/python-learnings.md)| January 3, 2019 	|
 | 1 | [Installing pip on Linux](posts/gsoc-proposal.md)					                | December 14, 2018	|
+
+
+# Code Structure
+
+The repo contains rust code to generate a `_posts.json` file that can be used to render on front-end of choice. I intend to write a CI flow to generate the same, maybe even generate single `<slug>.json` files and index them in the `_posts.json` if the number of posts are enough to demand such a work-around.
---
+++ src/main.rs
@@ -0,0 +1,63 @@
+use pulldown_cmark::{html, Options, Parser};
+use std::{
+    env,
+    fs::{read_dir, File},
+    io::{BufRead, BufReader, Error, Write},
+};
+
+fn main() -> Result<(), Error> {
+    let current_dir = env::current_dir()?;
+    let posts = read_dir(current_dir.join("posts"))?;
+    let mut output = vec![];
+
+    for file in posts {
+        let mut render = String::new();
+        let mut details = String::new();
+        let post = File::open(file?.path())?;
+        let buf = BufReader::new(post);
+        let (mut seeking_details, mut parsing_details) = (true, false);
+
+        for line in buf.lines() {
+            let line = line?;
+
+            if line == "---".to_string() && seeking_details {
+                seeking_details = false;
+                parsing_details = true;
+                continue;
+            } else if parsing_details {
+                if line == "---".to_string() {
+                    parsing_details = false;
+                } else {
+                    let (desc, body) = line.split_at(line.find(':').unwrap_or(0));
+                    details.push_str(&format!(
+                        "\t\t\t\"{}\": \"{}\",\n",
+                        desc,
+                        body.replace(":", "").trim().replace("'", "")
+                    ));
+                }
+                continue;
+            } else if seeking_details {
+                seeking_details = false;
+            }
+
+            let (mut html_output, parser) =
+                (String::new(), Parser::new_ext(&line, Options::empty()));
+            html::push_html(&mut html_output, parser);
+            render.push_str(&html_output.replace("\"", "\\\""));
+        }
+
+        output.push((details, render));
+    }
+
+    let mut json_file = File::create(current_dir.join("_posts.json"))?;
+    writeln!(json_file, "{{\n\t\"posts\": [")?;
+
+    for (details, render) in output {
+        let json = format!("{}\t\t\t\"body\": \"{}\",", details, render);
+        writeln!(json_file, "\t\t{{\n{}\n\t\t}},", json)?;
+    }
+
+    writeln!(json_file, "\t],\n}}")?;
+
+    Ok(())
+}
