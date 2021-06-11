7d699f9840a76e8b84f995a5bda304898f06b5bd
Devdutt Shenoi <devdutt@outlook.in>
2021-06-11 20:26:44 +0530

    Publish html version

---
+++ js/site.js
@@ -0,0 +1,8 @@
+window.onscroll = function () { myFunction() };
+
+function myFunction() {
+    let winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    let scrolled = (winScroll / height) * 100;
+    document.getElementById("myBar").style.width = scrolled + "%";
+}
